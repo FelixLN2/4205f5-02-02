18e5fc3d2abe19484a8fdba45e23ee681133970a
lng105 <lng105@outlook.com>
2023-09-06 15:38:13 -0400

    Navigation 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import React, { useState, useCallback } from "react";
 import logo from './logo.svg';
+
 import './App.css';
 import Accueil from "./Page/Accueil/Accueil"
 import Footer from './Shared/Components/Footer/Footer';
 
-function App() {
+const App = () =>{
   return (
     <div className="App">
       <Accueil/>
---
+++ frontend/src/Page/Stages/Stages.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Accueil.css";
+
+function Stages() {
+  return (
+    <div className="container-accueil">
+      <h3 className="h3-accueil">
+     dasdasdas
+      </h3>
+    </div>
+  );
+}
+
+export default Stages;
