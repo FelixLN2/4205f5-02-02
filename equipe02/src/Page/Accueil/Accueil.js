8e1cbee06d1c06e2a51ca903e51248f7f0b49430
lng105 <lng105@outlook.com>
2023-09-06 13:52:20 -0400

    Page Accueil barebones

---
+++ equipe02/src/Page/Accueil/Accueil.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Accueil.css";
+
+function Accueil() {
+  return (
+    <div className="container-accueil">
+      <h3 className="h3-accueil">
+        Bienvenue sur le site des stages de fin d'études des techniques de
+        l'informatique du Collège Montmorency!
+      </h3>
+    </div>
+  );
+}
+
+export default Accueil;
