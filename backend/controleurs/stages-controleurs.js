9323b97d576b9350c7ada1c8cc199ee820e34356
Félix Laprise-Narbonne <2215307@cmontmorency.qc.ca>
2023-10-18 16:01:54 -0400

    findOne

--- backend/controleurs/stages-controleurs.js
+++ backend/controleurs/stages-controleurs.js
@@ -125,7 +125,7 @@ const supprimerStage = async (requete, reponse, next) => {
     // faut enlever le stage de la liste de stages pour les etudiants et employeurs
     //
     await Etudiant.updateMany({listeStages: stageId}, { $pull: {listeStages: stageId}});
-    const employeur = await Employeur.find({identifiant: unStage.employeur_id});
+    const employeur = await Employeur.findOne({identifiant: unStage.employeur_id});
     await employeur.listeStages.pull(stageId);
 
     await Stage.findByIdAndRemove(stageId);
