0c76d8eb728132a9a612464f81030da9ddf02d22
Félix Laprise-Narbonne <2215307@cmontmorency.qc.ca>
2023-10-18 15:14:32 -0400

    Co-authored-by: Zac9742 <Zac9742@users.noreply.github.com>

--- backend/models/stage.js
+++ backend/models/stage.js
@@ -7,6 +7,12 @@ const stageSchema = new Schema({
     titre: {type: String, required: true},
     description: {type: String, required: true},
     employeur_id: {type: String, ref:"Employeur", required: true},
+    // debut:{type: String, required:true},
+    // fin: {type: String, required:true},
+    // payant: {type: Boolean, required:true},
+    // modalite: {type: String, required:true},
+    // entreprise: {type: String, required:true},
+    // status: {type: Boolean, required:true},
     listeEtudiants: [{type: String}]
 });
 
